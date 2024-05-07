ea4daa40016e5c45abb7a7cf81ef44bccb45ecd1
Samdwag <114537677+Samdwag@users.noreply.github.com>
2024-05-07 12:16:41 -0500

    Add project files.

---
+++ Track Meet/Models/Runner.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Track_Meet.Models;
+
+public partial class Runner
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [DisplayName("First Name")]
+    [Required(ErrorMessage = "First name is required.")]
+    [StringLength(50, ErrorMessage = "First name must be between {2} and {1} characters.", MinimumLength = 2)]
+    public string FirstName { get; set; } = null!;
+
+    [DisplayName("Last Name")]
+    [Required(ErrorMessage = "Last name is required.")]
+    [StringLength(50, ErrorMessage = "Last name must be between {2} and {1} characters.", MinimumLength = 2)]
+    public string LastName { get; set; } = null!;
+
+    [Required(ErrorMessage = "Gender is required.")]
+    [StringLength(10, ErrorMessage = "Gender must be either 'Male' or 'Female'.")]
+    public string Gender { get; set; } = null!;
+
+    public string Event { get; set; } = null!;
+
+    [DisplayName("Result (Time)")]
+    [Required(ErrorMessage = "Result is required.")]
+    [RegularExpression(@"^\d+(\.\d{1,2})?$", ErrorMessage = "Result must be a number with up to two decimal places.")]
+    public string Result { get; set; } = null!;
+}
---
+++ Track Meet/Models/RunnerContext.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Track_Meet.Models;
+
+public partial class RunnerContext : DbContext
+{
+    public RunnerContext()
+    {
+    }
+
+    public RunnerContext(DbContextOptions<RunnerContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Runner> Runners { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+           // optionsBuilder.UseSqlServer("Server=DESKTOP-BTAINQ0\\SQLEXPRESS;Database=RUNNER;Trusted_Connection=True;TrustServerCertificate=true");
+        }
+    }
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+//        => optionsBuilder.UseSqlServer("Server=DESKTOP-BTAINQ0\\SQLEXPRESS;Database=RUNNER;Trusted_Connection=True;TrustServerCertificate=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Runner>(entity =>
+        {
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("id");
+            entity.Property(e => e.Event)
+                .HasMaxLength(150)
+                .IsUnicode(false);
+            entity.Property(e => e.FirstName)
+                .HasMaxLength(150)
+                .IsUnicode(false);
+            entity.Property(e => e.Gender)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.LastName)
+                .HasMaxLength(150)
+                .IsUnicode(false);
+            entity.Property(e => e.Result)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
